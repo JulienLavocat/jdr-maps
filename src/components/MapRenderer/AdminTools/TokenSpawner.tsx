c402e1c285a543c87b5514a2667795b7113236c5
Julien Lavocat <julien.lavocat@gmail.com>
2021-05-08 23:11:43 +0200

    New tokens support

--- src/components/MapRenderer/AdminTools/TokenSpawner.tsx
+++ src/components/MapRenderer/AdminTools/TokenSpawner.tsx
@@ -1,26 +1,55 @@
 import React, { useContext, useState } from "react";
 
 import { CurrentRoomCtx } from "../../../pages/Room/index";
-import tokensIndex from "../tokens.json";
+import tokensIndex from "../tokens2.json";
 import { Form, Row, Col, InputGroup } from "react-bootstrap";
 
-const tokens: Record<
+type TokenIndex = Record<
 	string,
-	{ name: string; displayName: string; url: string }[]
-> = tokensIndex as Record<
-	string,
-	{ name: string; displayName: string; url: string }[]
+	Record<string, { name: string; displayName: string; url: string }[]>
 >;
 
-const categories: string[] = Object.keys(tokens);
+const tokens: TokenIndex = tokensIndex as TokenIndex;
+
+const mods: string[] = Object.keys(tokens);
 
 export default function TokenSpawner() {
 	const { addToken } = useContext(CurrentRoomCtx);
-	const [category, setCategory] = useState(categories[0]);
+	const [mod, setMod] = useState(mods[0]);
+	const [categories, setCategories] = useState<string[]>(
+		Object.keys(tokens[mod]),
+	);
+	const [category, setCategory] = useState<string>(
+		Object.keys(tokens[mod])[0],
+	);
 
 	return (
 		<Form>
 			<Row>
+				<Col lg={3}>
+					<InputGroup>
+						<InputGroup.Prepend>
+							<InputGroup.Text id="basic-addon1">
+								Mod
+							</InputGroup.Text>
+						</InputGroup.Prepend>
+						<Form.Control
+							as="select"
+							onChange={(e) => {
+								setMod(e.target.value);
+								const c = Object.keys(tokens[e.target.value]);
+								setCategories(c);
+								setCategory(c[0]);
+							}}
+						>
+							{mods.map((e) => (
+								<option key={e} value={e}>
+									{e}
+								</option>
+							))}
+						</Form.Control>
+					</InputGroup>
+				</Col>
 				<Col lg={3}>
 					<InputGroup>
 						<InputGroup.Prepend>
@@ -30,7 +59,9 @@ export default function TokenSpawner() {
 						</InputGroup.Prepend>
 						<Form.Control
 							as="select"
-							onChange={(e) => setCategory(e.target.value)}
+							onChange={(e) => {
+								setCategory(e.target.value);
+							}}
 						>
 							{categories.map((e) => (
 								<option key={e} value={e}>
@@ -53,11 +84,8 @@ export default function TokenSpawner() {
 								addToken([500, 500], e.target.value);
 							}}
 						>
-							{Object.values(tokens[category]).map((e) => (
-								<option
-									key={category + "/" + e.name}
-									value={e.url}
-								>
+							{Object.values(tokens[mod][category]).map((e) => (
+								<option key={mod + "/" + e.name} value={e.url}>
 									{e.displayName}
 								</option>
 							))}
