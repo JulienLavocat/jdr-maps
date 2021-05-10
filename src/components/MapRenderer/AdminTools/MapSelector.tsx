f0eb930bbdec104e99325ba3a86ece47b4a3d362
Julien Lavocat <julien.lavocat@gmail.com>
2021-05-10 18:21:32 +0200

    Added form to change map

--- src/components/MapRenderer/AdminTools/MapSelector.tsx
+++ src/components/MapRenderer/AdminTools/MapSelector.tsx
@@ -1,31 +1,57 @@
 import React, { useContext, useEffect, useState } from "react";
 import MapsAPI from "../../../utils/MapsAPI";
 import { CurrentRoomCtx } from "../../../pages/Room/index";
-import { Container, Form } from "react-bootstrap";
+import { Container, Form, Col, Button } from "react-bootstrap";
+import { MapPin } from "../MapPin";
 
 const API = "/api";
 
 export default function MapSelector() {
-	const { mapUrl, setMap } = useContext(CurrentRoomCtx);
-	const [maps, setMaps] = useState<string[]>([]);
+	const { setMap } = useContext(CurrentRoomCtx);
+	const [maps, setMaps] = useState<string[] | null>(null);
+	const [currentMap, setCurrentMap] = useState<string | null>(null);
+	console.log(currentMap);
 
 	useEffect(() => {
-		MapsAPI.getMaps().then((res) => {
-			setMaps(res.map((e: any) => e.name));
-		});
+		MapsAPI.getMaps()
+			.then((res) => {
+				const mapsNames = res.map((e: any) => e.name);
+				setMaps(mapsNames);
+				setCurrentMap(mapsNames[0]);
+			})
+			.catch((er) => console.error(er));
 
 		return () => {};
 	}, []);
 
 	return (
 		<Container fluid>
-			<Form.Control as="select" onChange={(e) => setMap(e.target.value)}>
-				{maps.map((e) => (
-					<option key={e} value={e}>
-						{e}
-					</option>
-				))}
-			</Form.Control>
+			{!maps ? (
+				<p>Fetching maps...</p>
+			) : (
+				<Form
+					onSubmit={() => {
+						if (!currentMap) return;
+						setMap(currentMap);
+					}}
+				>
+					<Col lg={3}>
+						<Form.Control
+							as="select"
+							onChange={(e) => setMap(e.target.value)}
+						>
+							{maps.map((e) => (
+								<option key={e} value={e}>
+									{e}
+								</option>
+							))}
+						</Form.Control>
+					</Col>
+					<Col lg={3}>
+						<Button variant="success">Change Map</Button>
+					</Col>
+				</Form>
+			)}
 		</Container>
 	);
 }
